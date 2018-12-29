d6add86379d81caf018c1abcdba8b73a9ba5d59e
Sil van Diepen <me@silvandiepen.com>
2018-12-29 20:46:55 +0100

    add text-align and remove ui- from names

--- build/icons.js
+++ build/icons.js
@@ -2,17 +2,18 @@ const sass = require('sass');
 const Fiber = require('fibers');
 const fs = require('fs');
 const path = require('path');
+
 const src = './src/icon';
 const dist = './dist/icon';
 
 function doSass(file) {
 	sass.render(
 		{
-			file: path.join(src, file),
+			data: '@import "src/mixins.scss"; @include silicon-' + file + '(); ',
 			fiber: Fiber
 		},
 		function(err, result) {
-			fs.writeFile(path.join(dist,file.replace('.scss', '.css')), result.css, (err) => {
+			fs.writeFile(path.join(dist, file + '.css'), result.css, (err) => {
 				if (err) throw err;
 				console.log(`${file} is created`);
 			});
@@ -20,9 +21,26 @@ function doSass(file) {
 	);
 }
 
-fs.readdirSync(src).forEach((file) => {
-	console.log(file);
-	if (file.indexOf('.scss') > 0) {
-		doSass(file);
+function makeJson(items) {
+	let iconList = {};
+	for (var i = 0; i < items.length; i++) {
+		if (items[i].indexOf('.scss') > 0) {
+			items[i] = items[i].replace('.scss', '');
+		}
 	}
+	iconList.silicons = items;
+	
+	fs.writeFile(path.join('./dist/','icons.json'), JSON.stringify(iconList), (err) => {
+		if (err) throw err;
+		console.log(`icons.json is created`);
+	});
+}
+
+fs.readdir(src, function(err, items) {
+	items.forEach(function(item) {
+		if (item.indexOf('.scss') > 0) {
+			doSass(item.replace('.scss', ''));
+		}
+	});
+	makeJson(items);
 });
