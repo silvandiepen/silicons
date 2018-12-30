8c083a5bfae415832eb058ba0f2aed6ed8a1bf26
Sil van Diepen <me@silvandiepen.com>
2018-12-30 14:53:33 +0100

    fixing comments and add coloring

--- build/icons.js
+++ build/icons.js
@@ -20,22 +20,28 @@ function doSass(file) {
 		}
 	);
 }
+function arrayRemove(arr, value) {
+	return arr.filter(function(ele) {
+		return ele != value;
+	});
+}
 
-function makeFiles(items) {
+function makeFiles(items, callback) {
 	let iconList = {};
+	iconList.silicons =[];
+	let iconArray = [];
 	let mixins = '';
-	let includes = '@import \'mixins\'; \n';
-	
+	let includes = "@import 'mixins'; \n";
+
 	for (var i = 0; i < items.length; i++) {
 		if (items[i].indexOf('.scss') > 0) {
 			let iconname = items[i].replace('.scss', '');
-			items[i] = iconname;
+			iconList.silicons.push(iconname);
 			mixins = mixins + `\n @import 'icon/${iconname}';`;
 			includes = includes + `@include silicon-${iconname}(); \n`;
 		}
 	}
-	iconList.silicons = items;
-
+	
 	// JSON
 	fs.writeFile(
 		path.join('./dist/', 'icons.json'),
@@ -47,34 +53,28 @@ function makeFiles(items) {
 	);
 
 	// MIXINS
+	fs.writeFile(path.join('./src/', 'mixins.scss'), mixins, (err) => {
+		if (err) throw err;
+		console.log(`mixins file is created`);
+	});
 
-	fs.writeFile(
-		path.join('./src/', 'mixins.scss'),
-		mixins,
-		(err) => {
-			if (err) throw err;
-			console.log(`mixins file is created`);
-		}
-	);
+	// ICONS
+	fs.writeFile(path.join('./src/', 'icons.scss'), includes, (err) => {
+		if (err) throw err;
+		console.log(`icons file is created`);
+	});
 
-	//
-	fs.writeFile(
-		path.join('./src/', 'icons.scss'),
-		includes,
-		(err) => {
-			if (err) throw err;
-			console.log(`icons file is created`);
-		}
-	);
+	callback();
 }
 
 fs.readdir(src, function(err, items) {
-
-	makeFiles(items);
-	
-	items.forEach(function(item) {
-		if (item.indexOf('.scss') > 0) {
-			doSass(item.replace('.scss', ''));
-		}
+	makeFiles(items, function() {
+		setTimeout(function() {
+			items.forEach(function(item) {
+				if (item.indexOf('.scss') > 0) {
+					doSass(item.replace('.scss', ''));
+				}
+			});
+		}, 1000);
 	});
 });
