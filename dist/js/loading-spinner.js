c196d8368316a85fb759310e8c2f87eb534b7fdc
Sil van Diepen <me@silvandiepen.com>
2019-01-06 18:42:25 +0100

    add loading-spinner

---
+++ dist/js/loading-spinner.js
@@ -0,0 +1,2 @@
+const data = {"loading-spinner":{"data":".silicon-loading-spinner {\n  /* position */\n  position: relative;\n  /* size */\n  width: 0.25em;\n  height: 0.25em;\n  /* background */\n  background-color: transparent;\n  /* border */\n  border-radius: 50%;\n  box-shadow: 0 -0.4em, -0.28em -0.28em 0 -0.01em, -0.4em 0 0 -0.02em, -0.28em 0.28em 0 -0.03em, 0 0.4em 0 -0.04em, 0.28em 0.28em 0 -0.05em, 0.4em 0 0 -0.06em, 0.28em -0.28em 0 -0.07em;\n  /* margin */\n  margin: 0.375em;\n}"}}
+	export default data;
